8f8f69d5560f4a7c050abd5a5acd36f826a3219c
Jakub Pelc <jakub.pelc@seznam.cz>
2024-03-04 21:45:45 +0100

    updated dotfiles

---
+++ NixOS/update.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+USR=jakub
+TIME="date +%Y-%m-%d-%H-%M-%S"
+nixos-rebuild switch
+
+exit_code=$?
+
+if [ $exit_code -eq 0 ]; then
+
+    cp -a /etc/nixos/. /home/jakub/Documents/dotfiles/NixOS
+    cd /home/jakub/Documents/dotfiles/NixOS
+    git add .
+    git commit -m "updated nixos dotfiles $TIME"
+    git push
+
+    echo "Synced config with git."
+else
+    echo "Error during building NixOS. $exit_code"
+fi
