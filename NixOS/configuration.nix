6bc8f0e1f0f5126b144ba8c5163d78b513b48452
Jakub Pelc <jakub.pelc@seznam.cz>
2024-08-02 12:24:16 +0200

    updated nixos dotfiles 2024-08-02-12-23-29

--- NixOS/configuration.nix
+++ NixOS/configuration.nix
@@ -95,7 +95,7 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  sound.enable = true;
+  #sound.enable = true;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -108,6 +108,17 @@
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
+  };
+
+  environment.etc = {
+	"wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
+		bluez_monitor.properties = {
+			["bluez5.enable-sbc-xq"] = true,
+			["bluez5.enable-msbc"] = true,
+			["bluez5.enable-hw-volume"] = true,
+			["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+		}
+	'';
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
